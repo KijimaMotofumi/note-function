de9fdca3406d87192545f37faa380995aa19a5c0
Kijima Motofumi <kijima@kllc.jp>
2026-01-02 15:06:58 +0900

    wip

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const { app } = require('@azure/functions');
 
+// Register HTTP triggers (app.http) by requiring the module(s).
+require('./functions/note-push');
+
 app.setup({
     enableHttpStream: true,
 });
